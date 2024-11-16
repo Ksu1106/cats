ce9cec8dd327a6000ef0cd2ac6f63ccd698f517d
Ksu1106 <skywolfmarkova11@gmail.com>
2024-11-16 23:12:57 +0300

    Создана кнопка обновления картинки с котиком

--- cats_image.py
+++ cats_image.py
@@ -1,13 +1,13 @@
 from http.client import responses
 from tkinter import *
 from PIL import Image, ImageTk
-import request
+import requests
 from io import BytesIO
 
 
-def load_image():
+def load_image(url):
     try:
-        response = request.get(url)
+        response = requests.get(url)
         response.raise_for_status()
         image_data = BytesIO(response.content)
         img = Image.open(image_data)
@@ -15,6 +15,16 @@ def load_image():
     except Exception as e:
         print(f'Произошла ошибка: {e}')
         return None
+
+def set_image():
+    img = load_image(url)
+    if img:
+        label.config(image=img)
+        label.image = img
+
+
+
+
 
 
 window = Tk()
@@ -24,6 +34,9 @@ window.geometry('600x480')
 label = Label()
 label.pack()
 
+update_button = Button(text='Обновить картинку', command=set_image)
+update_button.pack()
+
 url = 'https://cataas.com/cat'
 
 img = load_image(url)
@@ -31,5 +44,7 @@ img = load_image(url)
 if img:
     label.config(image=img)
     label.image = img
+
+set_image()
 
 window.mainloop()
