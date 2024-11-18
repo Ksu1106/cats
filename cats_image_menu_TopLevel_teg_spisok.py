7b284f72689f53609fc1a49279907b3e1ae1e700
Ksu1106 <skywolfmarkova11@gmail.com>
2024-11-18 15:03:08 +0300

    Добавлено комбобокс для выборов тегов. Поле Entry удалено

---
+++ cats_image_menu_TopLevel_teg_spisok.py
@@ -0,0 +1,64 @@
+from http.client import responses
+from tkinter import ttk
+from tkinter import *
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO
+
+Allowed_tags = ['slepp','jump','big','fight','black','white','cute','funny']
+
+def load_image(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        image_data = BytesIO(response.content)
+        img = Image.open(image_data)
+        img.thumbnail((600,480), Image.Resampling.LANCZOS)
+        return ImageTk.PhotoImage(img)
+    except Exception as e:
+        print(f'Произошла ошибка: {e}')
+        return None
+
+
+def open_new_window():
+    tag = tag_combobox.get()
+    url_tag = f'https://cataas.com/cat/{tag}' if tag else 'https://cataas.com/cat'
+    img = load_image(url_tag)
+
+    if img:
+        new_window = Toplevel()
+        new_window.title('Картинка с котиком')
+        new_window.geometry('600x480')
+        label = Label(new_window, image=img)
+        label.pack()
+        label.image = img
+
+
+def exit():
+    window.destroy()
+
+window = Tk()
+window.title('Cats!')
+window.geometry('800x600')
+
+menu_bar = Menu(window)
+window.config(menu = menu_bar)
+
+file_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label='Файл', menu = file_menu)
+file_menu.add_command(label='Загрузить фото', command = open_new_window)
+file_menu.add_separator()
+file_menu.add_command(label='Выход', command = exit)
+
+url = 'https://cataas.com/cat'
+
+tag_label = Label(text='Выбери тег')
+tag_label.pack()
+
+tag_combobox = ttk.Combobox(values=Allowed_tags)
+tag_combobox.pack()
+
+load_button = Button(text='Загрузить по тегу',command=open_new_window)
+load_button.pack()
+
+window.mainloop()
